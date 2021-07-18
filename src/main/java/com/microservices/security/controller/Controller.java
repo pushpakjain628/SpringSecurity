c6ed5892f4e62d9f1da473df6a99e4e195b7d1c2
iampsjain <iampsjain@gmail.com>
2021-07-18 19:05:27 +0530

    initial commit

---
+++ src/main/java/com/microservices/security/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.microservices.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello";
+	}
+}
